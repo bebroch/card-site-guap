5e4ec9ae877f3ad092c14ea03fb9a5e698b353f8
bebroch <cawapog@yandex.ru>
2024-09-28 19:29:11 +0300

    fix: public folder bug

--- src/components/body/body.tsx
+++ src/components/body/body.tsx
@@ -4,15 +4,15 @@ export default function Body() {
     return (
         <div className={styles.main}>
             <div className={styles.card}>
-                <img className={styles.images} src={'public/typescript-logo.jpg'} />
+                <img className={styles.images} src={'typescript-logo.jpg'} />
                 <a>Разрабатываю веб приложения на TypeScript.</a>
             </div>
             <div className={styles.card}>
-                <img className={styles.images} src={'public/heldent.png'} />
+                <img className={styles.images} src={'heldent.png'} />
                 <a>Разработал Backend на Хакатоне ZavodIT.</a>
             </div>
             <div className={styles.card}>
-                <img className={styles.images} src={'public/github.png'} />
+                <img className={styles.images} src={'github.png'} />
                 <a>Пишу свои собственные маленькие проекты.</a>
             </div>
         </div>
